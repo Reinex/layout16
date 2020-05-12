bcd99b87a96dff8df3afd0949cefd5370c1e3d78
Kalabukhav Viachaslav <37269039+Reinex@users.noreply.github.com>
2020-05-12 09:48:12 +0300

    Hamburget
    add & fix burger menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+import Query from './src/query.js';
 import Hamburger from './src/hamburger.js';
-const hamburgerNode = document.querySelector('.hamburger');
+const hamburgerNode = document.querySelector('.hamburger-menu');
 const hamburger = new Hamburger(hamburgerNode);
+hamburger.burgerMenu(hamburgerNode);
+
+const mainButton = document.querySelector('.main-section__button');
+const urlQuery = 'https://httpbin.org/status/200';
+const query = new Query(urlQuery);
+query.sendQuery(mainButton);
--- js/src/hamburger.js
+++ js/src/hamburger.js
@@ -1,10 +1,34 @@
-export default class Hamburger {
+
+export default class Hamgurger {
     constructor(hamburger) {
         this.hamburger = hamburger;
-
-        this.hamburger.addEventListener('click', e => this.click(e.currentTarget));
     }
-    click(item) {
-        item.classList.toggle('active');
+    burgerMenu(hamburger) {
+        let menu = hamburger;
+        let button = menu.querySelector(".hamburger-menu__button");
+        let links = menu.querySelector(".hamburger-menu__link");
+        let overlay = menu.querySelector(".hamburger-menu__overlay");
+
+
+        button.addEventListener("click", (e) => {
+            e.preventDefault();
+            toggleMenu();
+        });
+        [].forEach.call(links, function (el) {
+            el.addEventListener("click", () => toggleMenu());
+        });
+        overlay.addEventListener("click", () => toggleMenu());
+
+
+        function toggleMenu() {
+            menu.classList.toggle("hamburger-menu__active");
+            if (menu.classList.contains("hamburger-menu__active")) {
+                document.body.style.overflow = "hidden";
+            } else {
+                document.body.style.overflow = "visible";
+            }
+        }
     }
 }
+
+
